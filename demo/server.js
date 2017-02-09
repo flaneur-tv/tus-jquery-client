0cff61ec9d7162a1bf5ac15ca71400294737a332
Markus Kienast <mark@flaneur.tv>
2017-02-09 00:07:26 -0500

    Modifications to work with Docker

--- demo/server.js
+++ demo/server.js
@@ -4,13 +4,13 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(req, res) {
   if (req.url === '/') {
-    res.writeHead(301, {'Location': '/demo/index.html'});
+    res.writeHead(301, {'Location': '/index.html'});
     res.end();
     return;
   }
 
   send(req, req.url)
-    .root(__dirname+'/../')
+    .root(__dirname+'/')
     .pipe(res);
 }).listen(port);
 
